2fd5272fbf7982328ca4e4dce823581a40f6b2f3
Alexis Vergoz <77892858+AlexisVergoz@users.noreply.github.com>
2021-04-05 00:08:28 +0200

    Create our-beers.component.spec.ts

---
+++ src/app/home/our-beers/our-beers.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OurBeersComponent } from './our-beers.component';
+
+describe('OurBeersComponent', () => {
+  let component: OurBeersComponent;
+  let fixture: ComponentFixture<OurBeersComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ OurBeersComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(OurBeersComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
